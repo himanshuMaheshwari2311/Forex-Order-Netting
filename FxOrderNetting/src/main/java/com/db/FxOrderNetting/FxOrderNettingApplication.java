ecd3eb6540d8529183bfbce4733cb108d407eb74
shivangigupta1404 <shivangi.gupta1404@gmail.com>
2018-07-25 12:48:18 +0530

    initial spring boot app

---
+++ FxOrderNetting/src/main/java/com/db/FxOrderNetting/FxOrderNettingApplication.java
@@ -0,0 +1,12 @@
+package com.db.FxOrderNetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxOrderNettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxOrderNettingApplication.class, args);
+	}
+}
